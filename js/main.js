dd5ae41b2ae9fb81dde76ed881e3afefa7725584
lokeshdevdadws <130240998+lokeshdevdadws@users.noreply.github.com>
2023-05-21 13:50:46 +0530

    portfolio

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+$(document).ready(function(){
+    $(window).scroll(function(){
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+ 
+    
+});
+
+ AOS.init({
+	 duration: 1200
+ });
+
